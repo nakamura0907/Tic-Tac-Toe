48e5cb95f452dc9414530de2175dd5e3f44baebc
ritsuki0907 <ritsuki990907@gmail.com>
2020-12-01 18:44:01 +0900

    npm導入 test準備

---
+++ __tests__/main.test.js
@@ -0,0 +1,5 @@
+describe("changeSquare", () => {
+  it("1 -> ○", () => {
+    expect(1).toBe(10);
+  })
+})
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/script.js",
+  output: {
+    path: path.join(__dirname, ""),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              url: false
+            }
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+        exclude: [/node_modules/, require.resolve("./src/index.html")]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+}
